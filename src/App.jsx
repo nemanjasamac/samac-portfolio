565e05e56a48b300c9631544d0f9dd41a2b40ae3
Nemanja Samac <samac.dev@gmail.com>
2025-04-19 15:24:27 +0200

    Initial commit: "Setup React + Tailwind CSS"

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-blue-950 to-indigo-600 text-white flex flex-col items-center justify-center">
+      <h1 className="text-5xl font-bold mb-4">Tailwind is Working 🎉</h1>
+      <p className="text-lg">This is a custom React + Tailwind starter.</p>
+    </div>
+  );
+}
